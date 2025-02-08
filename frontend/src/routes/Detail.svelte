4f15229e6b6a8095b889224d743d5200c8d81b6e
tomato <ahsdfg30@gmail.com>
2025-02-08 19:32:29 +0900

    Feat(domain/answer, frontend): Implement answer creation functionality in backend and frontend

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -3,27 +3,29 @@
 
     export let params = {}
     let question_id = params.question_id
-    let question = {}
-    let content = {}
+    let question = {answers: []}
+    let content = ""
 
-    function get_question() {
+   function get_question() {
         fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
-                question = json
-            })
-        }
+            question = json
+        })
+    }
 
     get_question()
 
     function post_answer(event) {
         event.preventDefault()
-        let url = "/api/answer/create" + question_id
+        let url = "/api/answer/create/" + question_id
         let params = {
             content: content
         }
-        fastapi('post', url, params, (json) => {
-            content = ''
-            get_question()
-        })
+        fastapi('post', url, params, 
+            (json) => {
+                content = ''
+                get_question()
+            }
+        )
     }
 </script>
 
@@ -31,6 +33,11 @@
 <div>
     {question.content}
 </div>
+<ul>
+    {#each question.answers as answer}
+        <li>{answer.content}</li>
+    {/each}
+</ul>
 <form method="post">
     <textarea rows="15" bind:value={content}></textarea>
     <input type="submit" value="답변등록" on:click="{post_answer}">
