dd6832e36af00a399e128d93bfeca4ed7064d5ab
tomato <ahsdfg30@gmail.com>
2025-02-08 16:49:40 +0900

    Feat: Add dependency injection for database session management

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,3 +11,12 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+@contextlib.contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
