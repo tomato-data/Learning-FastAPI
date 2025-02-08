b04fbd9946f2cd79b25a3f7bac7ee58c7f955845
tomato <ahsdfg30@gmail.com>
2025-02-09 03:33:30 +0900

    Feat: Implement user registration functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from domain.answer import answer_router
 from domain.question import question_router
+from domain.user import user_router
 
 app = FastAPI()
 
@@ -22,5 +23,4 @@ app.add_middleware(
 
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
-for route in app.routes:
-    print(route.path, route.methods)
+app.include_router(user_router.router)
