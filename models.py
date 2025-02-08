38411dd1d1abffc6f88a7d86e872d0647eeb66f7
tomato <ahsdfg30@gmail.com>
2025-02-08 15:00:36 +0900

    Add(models): add models.py to project

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+from database import Base
+
+
+class Qustion(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
