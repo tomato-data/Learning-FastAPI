528867903e343f49c6000b832163c63591fb81b9
tomato <ahsdfg30@gmail.com>
2025-02-08 16:59:14 +0900

    Refactor(domain/question/question_router): change question_list's dependency injection to use fastapi's Depends

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Question
@@ -9,7 +10,6 @@ router = APIRouter(
 
 
 @router.get("/list")
-def question_list():
-    with get_db() as db:
-        _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+def question_list(db: Session = Depends(get_db)):
+    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return _question_list
