ab96d53f54560cdb6921db7a923fccd9a94fd61d
tomato <ahsdfg30@gmail.com>
2025-02-10 06:42:17 +0900

    Feat(Detail): Add '삭제' button for deleting answer

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -54,6 +54,23 @@
             )
         }
     }
+    
+    function delete_answer(answer_id) {
+        if(window.confirm('정말로 삭제하시겠습니까?')) {
+            let url = "/api/answer/delete"
+            let params = {
+                answer_id: answer_id
+            }
+            fastapi('delete', url, params,
+                (json) => {
+                    get_question()
+                },
+                (err_json) => {
+                    error = err_json
+                }
+            )
+        }
+    }
 </script>
 
 <div class="container my-3">
@@ -94,6 +111,7 @@
                 {#if answer.user && $username === answer.user.username }
                 <a use:link href="/answer-modify/{answer.id}" 
                     class="btn btn-sm btn-outline-secondary">수정</a>
+                <button class="btn btn-sm btn-outline-secondary" on:click={() => delete_answer(answer.id) }>삭제</button>
                 {/if}
             </div>
             </div>
