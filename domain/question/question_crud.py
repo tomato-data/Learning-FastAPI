38e4a812c7ba6436356f148a4712dcfa08361599
tomato <ahsdfg30@gmail.com>
2025-02-10 05:06:50 +0900

    Feat(domain, databse, models): Implement feature to save User information for questions

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from domain.question.question_schema import QuestionCreate
-from models import Question
+from models import Question, User
 from sqlalchemy.orm import Session
 
 
@@ -17,11 +17,12 @@ def get_question(db: Session, question_id: int):
     return question
 
 
-def create_question(db: Session, question_create: QuestionCreate):
+def create_question(db: Session, question_create: QuestionCreate, user: User):
     db_question = Question(
         subject=question_create.subject,
         content=question_create.content,
         create_date=datetime.now(),
+        user=user,
     )
     db.add(db_question)
     db.commit()
