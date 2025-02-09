00604959e374068721a1cfa9dfbe0ec0720b0493
tomato <ahsdfg30@gmail.com>
2025-02-10 05:44:21 +0900

    Feat(domain/question): Implement feature to modify question

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from domain.question.question_schema import QuestionCreate
+from domain.question.question_schema import QuestionCreate, QuestionUpdate
 from models import Question, User
 from sqlalchemy.orm import Session
 
@@ -24,5 +24,15 @@ def create_question(db: Session, question_create: QuestionCreate, user: User):
         create_date=datetime.now(),
         user=user,
     )
+    db.add(db_question)
+    db.commit()
+
+
+def update_question(
+    db: Session, db_question: Question, question_update: QuestionUpdate
+):
+    db_question.subject = question_update.subject
+    db_question.content = question_update.content
+    db_question.modify_date = datetime.now()
     db.add(db_question)
     db.commit()
--- domain/question/question_schema.py
+++ domain/question/question_schema.py
@@ -29,3 +29,7 @@ class QuestionCreate(BaseModel):
 class QuestionList(BaseModel):
     total: int = 0
     question_list: list[Question] = []
+
+
+class QuestionUpdate(QuestionCreate):
+    question_id: int
