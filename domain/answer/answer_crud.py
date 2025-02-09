629959c197c0501680bdf50332bea7a83d96ab8e
tomato <ahsdfg30@gmail.com>
2025-02-10 06:24:42 +0900

    Feat(domain/answer): Implement feature to modify answer

--- domain/answer/answer_crud.py
+++ domain/answer/answer_crud.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from sqlalchemy.orm import Session
 
-from domain.answer.answer_schema import AnswerCreate
+from domain.answer.answer_schema import AnswerCreate, AnswerUpdate
 from models import Question, Answer, User
 
 
@@ -15,5 +15,16 @@ def create_answer(
         create_date=datetime.now(),
         user=user,
     )
+    db.add(db_answer)
+    db.commit()
+
+
+def get_answer(db: Session, answer_id: int):
+    return db.query(Answer).get(answer_id)
+
+
+def update_answer(db: Session, db_answer: Answer, answer_update: AnswerUpdate):
+    db_answer.content = answer_update.content
+    db_answer.modify_date = datetime.now()
     db.add(db_answer)
     db.commit()
--- domain/answer/answer_schema.py
+++ domain/answer/answer_schema.py
@@ -19,3 +19,8 @@ class Answer(BaseModel):
     content: str
     create_date: datetime.datetime
     user: User | None
+    question_id: int
+
+
+class AnswerUpdate(AnswerCreate):
+    answer_id: int
